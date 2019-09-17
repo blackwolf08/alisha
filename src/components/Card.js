3f7870f95dcfa55ed5e7dff3e6c59496b16d5d66
Sunny Dhama <31814079+blackwolf08@users.noreply.github.com>
2019-09-17 20:45:54 +0530

    phase-1

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import Reveal from 'react-reveal/Reveal';
 
 const Card = ({url}) => {
     return (
-        <div className="card--root">
-            <img className="card__img" alt="Portfilio" src={url}></img>
-        </div>
+            <div className="card--root">
+                        <Reveal effect="img_in_vp" effectOut="img_not_in_vp">
+
+                
+                <img className="card__img" alt="Portfilio" src={url}></img>
+                    </Reveal>
+
+            </div>
+
     )
 }
 
